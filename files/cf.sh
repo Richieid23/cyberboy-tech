18e11ce7e548308dd156e4ded0d1f2c44baf9aa3
Richi <fitrarizki23@gmail.com>
2024-10-14 14:38:06 +0700

    update cloudflare

--- files/cf.sh
+++ files/cf.sh
@@ -9,31 +9,26 @@ echo ""
 echo ""
 #sub=$(</dev/urandom tr -dc a-z0-9 | head -c3)
 read -rp "Input Domain Name. Example ( Vpnku ): " -e sub
-DOMAIN=kingvpn.my.id
-SUB_DOMAIN=${sub}.kingvpn.my.id
-CF_ID=hannaugo@gmail.com
-CF_KEY=e3341a6705e970eda3577f440d0cca6e3d682
+DOMAIN=amaq-najwa.my.id
+SUB_DOMAIN=${sub}.amaq-najwa.my.id
+CF_KEY=k-I66cU8tkmUxUNg3XxMBq66ha-pZhCMpHPmxerT
 set -euo pipefail
 IP=$(curl -sS ifconfig.me);
 echo "Updating DNS for ${SUB_DOMAIN}..."
 ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
--H "X-Auth-Email: ${CF_ID}" \
--H "X-Auth-Key: ${CF_KEY}" \
+-H "Authorization: Bearer ${CF_KEY}" \
 -H "Content-Type: application/json" | jq -r .result[0].id)
 RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${SUB_DOMAIN}" \
--H "X-Auth-Email: ${CF_ID}" \
--H "X-Auth-Key: ${CF_KEY}" \
+-H "Authorization: Bearer ${CF_KEY}" \
 -H "Content-Type: application/json" | jq -r .result[0].id)
 if [[ "${#RECORD}" -le 10 ]]; then
 RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
--H "X-Auth-Email: ${CF_ID}" \
--H "X-Auth-Key: ${CF_KEY}" \
+-H "Authorization: Bearer ${CF_KEY}" \
 -H "Content-Type: application/json" \
 --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
 fi
 RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
--H "X-Auth-Email: ${CF_ID}" \
--H "X-Auth-Key: ${CF_KEY}" \
+-H "Authorization: Bearer ${CF_KEY}" \
 -H "Content-Type: application/json" \
 --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
 
